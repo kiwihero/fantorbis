c1571cc699b176d23d4d289ad1e27b35d2e2934b
mancinia754 <77806380+mancinia754@users.noreply.github.com>
2021-03-16 18:55:13 -0400

    Delete backendworld directory

--- backendworld/TectonicBoundary.py
+++
@@ -1,7 +0,0 @@
-from backendworld.WorldAttribute import WorldAttribute
-
-class TectonicBoundary(WorldAttribute):
-    def __init__(self, **kwargs):
-        self.origin = None
-        self.destination = None
-        super(TectonicBoundary, self).__init__(**kwargs)
