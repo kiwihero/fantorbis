92fffa18ef8e2f54b1449a93298d96790afd2eb0
Anne <mancinia754@strose.edu>
2021-03-22 16:44:34 -0400

    comments added for clarity

--- Conf.py
+++ Conf.py
@@ -2,10 +2,21 @@ import importlib
 
 class Conf:
     def __init__(self):
+        # You can put whatever here it doesn't have to be century gothic
         self.fontLocation = '/Users/mancia2/PycharmProjects/SoftwareEngMaps/fantorbis/07558_CenturyGothic.ttf'
+
+        # Change this to change how the data is stored
         self.structureClass = 'GridStructure'
         self.structureModule = 'backendstorage.GridStructure'
 
+        self.gifName = 'out.gif'
+
+    # ------------------------------------
+    # Functions needed for conf to work
+    # ------------------------------------
+
+    # If you know the name of the class and the module/package of the class
+    # Thanks stackoverflow https://stackoverflow.com/questions/1176136/convert-string-to-python-class-object
     def class_for_name(self, module_name, class_name):
         # load the module, will raise ImportError if module cannot be loaded
         m = importlib.import_module(module_name)
