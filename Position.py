0dae5f120578a163467a378a7a3d2e5ab036c75c
Anne <mancinia754@strose.edu>
2021-03-28 11:56:59 -0400

    Position class

---
+++ Position.py
@@ -0,0 +1,7 @@
+class Position:
+    """
+    The position functionality
+    """
+    def __init__(self):
+        self.x = None
+        self.y = None
--- backendstorage/Cells/Cell.py
+++ backendstorage/Cells/Cell.py
@@ -1,16 +1,24 @@
-# class Cell:
-#     def __init__(self):
-#         self.dataStoragePosition = None
-#         self.worldPosition = None
-#         self.vertexPoints = set()
+from Position import Position
+
+class Cell:
+    def __init__(self):
+        self.dataStoragePosition = Position()
+        self.worldPosition = Position()
+        self.vertexPoints = set()
 #         self._dataStorageStructure = None
 #         self._worldStructure = None
 #
-#     def move(self, newWorldPosition=None, newDataStoragePosition=None):
-#         if newWorldPosition is not None:
-#             self.worldPosition = newWorldPosition
-#         if newDataStoragePosition is not None:
-#             self.dataStoragePosition = newDataStoragePosition
+    def move(self, newWorldPosition=None, newDataStoragePosition=None):
+        """
+        Change the position known by the cell in either world or data storage coordinate systems
+        :param newWorldPosition: (x,y
+        :param newDataStoragePosition:
+        :return:
+        """
+        if newWorldPosition is not None:
+            self.worldPosition = newWorldPosition
+        if newDataStoragePosition is not None:
+            self.dataStoragePosition = newDataStoragePosition
 #
 #     def splitCellAlongVertex(self, vertexSegment):
 #         vertexSegment.splitVertex()
