0a3c789dcff05fdaa1ba3d09366e08cd248f20b0
Ashley Freels <ashnfreels@gmail.com>
2021-04-11 18:40:09 -0400

    added menu layout and display

--- Interface.py
+++ Interface.py
@@ -25,14 +25,49 @@ else:
 
     w1 = World()
 
-    root = tk.Tk()
-    root.wm_title('Fantorbis')
-    root.iconbitmap(os.path.join(sys.path[0], 'images/Fantorbis-Logo.ico'))
+
+    # class for menu layout
+    class MapMenu(tk.Frame):
+
+        def __init__(self):
+            super().__init__()
+
+            self.MapUI()
+
+        def MapUI(self):
+            self.pack(fill=BOTH, expand=True)
+
+            self.columnconfigure(1, weight=1)
+            self.columnconfigure(3, pad=7)
+            self.rowconfigure(3, weight=1)
+            self.rowconfigure(5, pad=7)
+            self.configure(bg='firebrick3')
+
+            area = Text(self)
+            area.grid(row=1, column=0, columnspan=2, rowspan=4,
+                      padx=5, sticky=E + W + S + N)
+
+            ## create_button = ttk.Button(self, text='Create World', command=World.random_wiggle())
+            ## create_button.grid(row=1, column=3)
+
+            ## step_button = ttk.Button(self, text='Step World', command=World.step(self))
+            ## step_button.grid(row=2, column=3)
+
+            saveMap_button = ttk.Button(self, text='Save Map', command=gif_world(w1))
+            saveMap_button.grid(row=3, column=3)
+
+            exit_button = ttk.Button(self, text='Exit', command=lambda: self.quit())
+            exit_button.grid(row=5, column=3)
 
 
+    def main():
+
+        root = Tk()
+        root.title('Fantorbis')
+        app = MapMenu()
+        root.iconbitmap(os.path.join(sys.path[0], 'images/Fantorbis-Logo.ico'))
+        root.mainloop()
 
 
-    saveMap_button = ttk.Button(root, text='Save Map', command=gif_world(w1)).grid(row=0, column=0)
-    exit_button = ttk.Button(root,text='Exit',command=lambda: root.quit()).grid(row=0, column=3)
-
-root.mainloop()
+    if __name__ == '__main__':
+        main()
