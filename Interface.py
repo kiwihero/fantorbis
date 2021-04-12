40b18bcaa1afc977a09ce1ae637635ab8ce59aa1
Ashley Freels <ashnfreels@gmail.com>
2021-04-12 12:53:09 -0400

    added menu layout and display

--- Interface.py
+++ Interface.py
@@ -14,9 +14,9 @@ from backendworld.World import *
 from PillowDisplay import draw_world, gif_world,image_world
 import os
 import sys
-import tkinter as tk
+from PIL import Image
+from tkinter import ttk, Tk
 from tkinter import *
-from tkinter import ttk
 # check if user has a older version of python
 if sys.version_info[0] == 2:
     import Tkinter as tk
@@ -26,8 +26,8 @@ else:
     w1 = World()
 
 
-    # class for menu layout
-    class MapMenu(tk.Frame):
+    # class for main window Frame
+    class MainWindow(tk.Frame):
 
         def __init__(self):
             super().__init__()
@@ -35,7 +35,7 @@ else:
             self.MapUI()
 
         def MapUI(self):
-            self.pack(fill=BOTH, expand=True)
+            self.pack(fill=tk.BOTH, expand=True)
 
             self.columnconfigure(1, weight=1)
             self.columnconfigure(3, pad=7)
@@ -43,18 +43,21 @@ else:
             self.rowconfigure(5, pad=7)
             self.configure(bg='firebrick3')
 
+            # area for map display
+            # use Image.open() to load image
             area = Text(self)
+            # area = Image.open(self)
             area.grid(row=1, column=0, columnspan=2, rowspan=4,
-                      padx=5, sticky=E + W + S + N)
+                      padx=5, sticky=tk.E + tk.W + tk.S + tk.N)
 
-            ## create_button = ttk.Button(self, text='Create World', command=World.random_wiggle())
-            ## create_button.grid(row=1, column=3)
+            create_button = ttk.Button(self, text='Create World', command=World())
+            create_button.grid(row=1, column=3)
 
-            ## step_button = ttk.Button(self, text='Step World', command=World.step(self))
-            ## step_button.grid(row=2, column=3)
+            step_button = ttk.Button(self, text='Step World', command=World.step(w1))
+            step_button.grid(row=3, column=3)
 
             saveMap_button = ttk.Button(self, text='Save Map', command=gif_world(w1))
-            saveMap_button.grid(row=3, column=3)
+            saveMap_button.grid(row=4, column=3)
 
             exit_button = ttk.Button(self, text='Exit', command=lambda: self.quit())
             exit_button.grid(row=5, column=3)
@@ -64,7 +67,7 @@ else:
 
         root = Tk()
         root.title('Fantorbis')
-        app = MapMenu()
+        app = MainWindow()
         root.iconbitmap(os.path.join(sys.path[0], 'images/Fantorbis-Logo.ico'))
         root.mainloop()
 
