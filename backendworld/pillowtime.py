a4fad89a0d8362ef34555cba6f9a11f5a874f9c1
kitty <petersk354@strose.edu>
2021-04-23 23:36:49 -0400

    working on pil

---
+++ backendworld/pillowtime.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+#converts matplotlib figure to PIL image and returns image.
+
+def pillow(): #include the thing taking in
+
+    x = np.arrange()
+    plt.plot(x)
+    fig = plt.gcf()
+
+    img = fig2img(fig)
+
+    img.show()
+
+
+
